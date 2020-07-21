94f2a438482714d22d428a65b70b8369f7899c0a
Angel-QI <984790699@qq.com>
2020-07-21 09:30:43 +0800

    mms

--- mms/src/App.js
+++ mms/src/App.js
@@ -1,24 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Layout, Menu, Breadcrumb, Typography } from 'antd';
+import { UserOutlined, LaptopOutlined, ShoppingOutlined, PropertySafetyOutlined } from '@ant-design/icons';
+
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
+const { Title } = Typography;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Header className="header">
+          <Title level={3} style={{ color: 'white', float: 'left', paddingTop: '14px' }}>CF周边后台管理系统</Title>
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" style={{ float: 'right' }}>
+            <Menu.Item key="1">登录</Menu.Item>
+            <Menu.Item key="2">注册</Menu.Item>
+          </Menu>
+        </Header>
+        <Layout>
+          <Sider width={200} className="site-layout-background">
+            <Menu
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['sub1']}
+              style={{ height: '100%', borderRight: 0 }}
+            >
+              <SubMenu key="sub1" icon={<UserOutlined />} title="首页">
+              </SubMenu>
+              <SubMenu key="sub2" icon={<PropertySafetyOutlined />} title="会员管理">
+              </SubMenu>
+              <SubMenu key="sub3" icon={<ShoppingOutlined />} title="商品管理">
+              </SubMenu>
+              <SubMenu key="sub4" icon={<LaptopOutlined />} title="订单管理">
+              </SubMenu>
+            </Menu>
+          </Sider>
+          <Layout style={{ padding: '10px' }}>
+            <Content
+              className="site-layout-background"
+              style={{
+                padding: 24,
+                margin: 0,
+                minHeight: 280,
+              }}
+            >
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>,
     </div>
   );
 }
---
+++ mms/src/pages/Plus/Plus.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+
+
+class Plus extends Component{
+    render(){
+
+        return (
+            <div>
+                Plus
+            </div>
+        )
+    }
+}
+
+export default Plus;
